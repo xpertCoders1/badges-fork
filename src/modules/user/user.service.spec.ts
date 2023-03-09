dae972ca8b5ab187f798a993cbb7f4741678d296
Vivek <vivekraj78965.vr@gmail.com>
2023-03-09 18:56:10 +0530

    test case of service for creating & saving user

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getCustomRepositoryToken, getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  const mockUserRepository = {
+    create:jest.fn().mockImplementation(dto => dto),
+    save:jest.fn().mockImplementation(user => Promise.resolve({id:Date.now() , ...user}))
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService , {
+        provide:getRepositoryToken(User),
+        useValue:mockUserRepository
+      }],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -15,4 +24,11 @@ describe('UserService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+  it('should create new user record  and return that' , async () => {
+    // const id = Date.now()
+    expect(await service.create({name : 'jhon'})).toEqual({
+      id:expect.any(Number),
+      name:'jhon'
+    })
+  })
 });
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 @Injectable()
 export class UserService {
+  constructor(@InjectRepository(User) private userRepo:Repository<User>){};
+  
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const newUser = this.userRepo.create(createUserDto);
+    return this.userRepo.save(newUser);
   }
 
   findAll() {
