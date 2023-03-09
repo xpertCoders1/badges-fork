15b114e36a34e5739e86ed6a6ded4680e7a52e2f
Vivek <vivekraj78965.vr@gmail.com>
2023-03-09 11:51:10 +0530

    unit testing for creation of an user

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -4,12 +4,20 @@ import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  const mockUserService = {
+    create:jest.fn(dto => {
+      return {
+        id:Date.now(),
+        ...dto
+      }
+    }) 
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
       providers: [UserService],
-    }).compile();
+    }).overrideProvider(UserService).useValue(mockUserService).compile();
 
     controller = module.get<UserController>(UserController);
   });
@@ -17,4 +25,10 @@ describe('UserController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+  it('should create a user' , () => {
+    expect(controller.create({name:'jhon'})).toEqual({
+      id:expect.any(Number),
+      name:'jhon'
+    })
+  })
 });
